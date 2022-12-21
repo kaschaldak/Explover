689ea72a6e9e10e6e2c7dc50761bf776ee010b90
kaschaldak <120067174+kaschaldak@users.noreply.github.com>
2022-12-22 00:14:05 +0500

    hello world

--- Explover 001/Program.cs
+++ Explover 001/Program.cs
@@ -1,14 +1,20 @@
-﻿void FillArray(int[] collection);
+﻿void infoarray(int[] collection);
 {
     int length = collection.length;
     int index = 0;
     while (index<length);
     {
-        collection[index] = new Random().Next(1,10);
+    
+        collection [index] = new Random().Next(1,10);
         //index = index + 1;
         index++;
 
     }
 
 }
+
+internal class collection
+{
+}
+
 int[] array = new int[10];
