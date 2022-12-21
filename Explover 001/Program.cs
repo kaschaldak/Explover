6bf6148f42724039482bb0420ab58bfc8ad6428a
kaschaldak <120067174+kaschaldak@users.noreply.github.com>
2022-12-22 01:23:06 +0500

    explover 001

--- Explover 001/Program.cs
+++ Explover 001/Program.cs
@@ -1,20 +1,2 @@
-﻿void infoarray(int[] collection);
-{
-    int length = collection.length;
-    int index = 0;
-    while (index<length);
-    {
-    
-        collection [index] = new Random().Next(1,10);
-        //index = index + 1;
-        index++;
-
-    }
-
-}
-
-internal class collection
-{
-}
-
-int[] array = new int[10];
+﻿Console.WriteLine ("Hay Hello,Word");
+var username = Console.ReadLine();
